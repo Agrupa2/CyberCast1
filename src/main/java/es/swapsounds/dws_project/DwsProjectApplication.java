97923b808f61d16ea1601df54b6bfb20673787b5
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-02-11 10:22:04 +0100

    Primer commit - Subiendo proyecto Spring Boot

---
+++ src/main/java/es/swapsounds/dws_project/DwsProjectApplication.java
@@ -0,0 +1,13 @@
+package es.swapsounds.dws_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/swapsounds/dws_project/DwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package es.swapsounds.dws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
