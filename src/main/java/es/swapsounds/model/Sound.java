a83bdd0248a90b31ac18b547364d11cfaaab6c6e
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-02-20 12:52:54 +0100

    Adding a new Sound constructor and id, and categoryID properties

--- src/main/java/es/swapsounds/model/Sound.java
+++ src/main/java/es/swapsounds/model/Sound.java
@@ -5,20 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sound {
+    private int id;
     private String title;
     private String description;
     private String filePath;
     private String imagePath;
     private User uploader;
     private List<Comment> comments;
+    private int CategoryId;
 
-    public Sound(String title, String description, String filePath, String imagePath, User uploader) {
+    public Sound(String title, String description, String filePath, String imagePath, User uploader, int CategoryId) {
         this.title = title;
         this.description = description;
         this.filePath = filePath;
         this.imagePath = imagePath;
         this.uploader = uploader;
         this.comments = new ArrayList<>();
+        this.CategoryId = CategoryId;
+    }
+
+    public Sound(int id, String title, String description, String filePath, String imagePath){
+        this.title = title;
+        this.id = id;
+        this.description = description;
+        this.filePath = filePath;
+        this.imagePath = imagePath;
     }
 
 
@@ -29,4 +40,6 @@ public class Sound {
     public String getImagePath() { return imagePath; }
     public User getUploader() { return uploader; }
     public List<Comment> getComments() { return comments; }
+    public int getId() { return id; }
+    public int getCategoryId() { return CategoryId; }
 }
