7457989c8bc2ca3e645cf42167eae8dafd99290b
miguel :) <me.delpino.2023@alumnos.urjc.es>
2025-03-03 23:39:11 +0100

    new getters and setters

--- src/main/java/es/swapsounds/model/Comment.java
+++ src/main/java/es/swapsounds/model/Comment.java
@@ -3,18 +3,12 @@ package es.swapsounds.model;
 import java.time.LocalDateTime;
 
 public class Comment {
-    private String id; //sirve para poder identificar un comentario. 
-    private User user; //usuario que pone el comentario
-    private String content; //contenido del comentario
-    private Sound sound; //sonido al que se refiere el comentario --> no se si habrá que usarlo
+    private String id; // sirve para poder identificar un comentario.
+    private User user; // usuario que pone el comentario
+    private String content; // contenido del comentario
+    private Sound sound; // sonido al que se refiere el comentario --> no se si habrá que usarlo
     private LocalDateTime created; // Fecha de creación
     private LocalDateTime modified; // Fecha de modificación (si se edita)
-
-    /*public Comment(User user, String content, Sound sound) {
-        this.user = user;
-        this.content = content;
-        this.sound = sound;
-    }*/
 
     public Comment(String id, String content, User user) {
         this.id = id;
@@ -37,12 +31,24 @@ public class Comment {
         return sound;
     }
 
-    public String getId(){
+    public void setSound(Sound sound) {
+        this.sound = sound;
+    }
+
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     public LocalDateTime getModified() {
@@ -56,5 +62,9 @@ public class Comment {
 
     public void setModified(LocalDateTime modified) {
         this.modified = modified;
+    }
+
+    public int getAuthorId() {
+        return this.user.getUserId(); // Asume que User tiene getUserId()
     }
 }
