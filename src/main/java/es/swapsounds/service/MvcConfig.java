f6c9ad8b23d85cccdee3cdf291438f4980ec5d3c
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-03-04 00:18:00 +0100

    changes to address static resources

--- src/main/java/es/swapsounds/service/MvcConfig.java
+++ src/main/java/es/swapsounds/service/MvcConfig.java
@@ -9,10 +9,20 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Configuración para uploads y avatares
         registry
-                .addResourceHandler("/uploads/**")
-                .addResourceLocations(
-                        "file:uploads/",
-                        "classpath:/static/default-avatars/");
+            .addResourceHandler("/uploads/**")
+            .addResourceLocations(
+                "file:uploads/",
+                "classpath:/static/default-avatars/");
+
+        // Configuración para recursos estáticos estándar (CSS/JS/Images)
+        registry
+            .addResourceHandler("/**")
+            .addResourceLocations(
+                "classpath:/static/css/",
+                "classpath:/static/js/", 
+                "classpath:/static/images/",
+                "classpath:/static/");
     }
 }
