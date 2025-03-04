7ec33d70d198a0bb45f2c2ba5a4435f619da882a
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-03-04 11:47:09 +0100

    Error status code change

--- src/main/java/es/swapsounds/controller/GlobalExceptionHandler.java
+++ src/main/java/es/swapsounds/controller/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex, Model model) {
         ModelAndView mav = new ModelAndView("error");
-        mav.addObject("status", 500);
+        mav.addObject("status", 404);
         mav.addObject("error", "Internal Server Error");
         mav.addObject("message", ex.getMessage());
         return mav;
