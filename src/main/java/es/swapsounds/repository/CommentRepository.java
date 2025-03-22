88df1948c0402e3519f236b2503be15dfc85dfda
4lex <a.gonzalezmart.2019@alumnos.urjc.es>
2025-03-22 18:04:18 +0100

    new jdk version

--- src/main/java/es/swapsounds/repository/CommentRepository.java
+++ src/main/java/es/swapsounds/repository/CommentRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.swapsounds.model.Comment;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    
+public interface CommentRepository extends JpaRepository<Comment, Long> { 
+    List<Comment> findBySoundId(Long soundId);
 }
