9f49eeb6fc0aa181b8326fa0ab0a2a2bad645dd9
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-03-22 17:17:03 +0100

    New file definition with LOB's

--- src/main/java/es/swapsounds/model/Sound.java
+++ src/main/java/es/swapsounds/model/Sound.java
@@ -15,8 +15,14 @@ public class Sound {
 
     private String title;
     private String description;
-    private String filePath;
-    private String imagePath;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] audioFile;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageFile;
 
     @ManyToOne
     private User user;
@@ -25,11 +31,11 @@ public class Sound {
     private String duration;
     private LocalDateTime uploadDate;
 
-    public Sound(String title, String description, String filePath, String imagePath, User user, String duration) {
+    public Sound(String title, String description, byte[] imageFile, byte[] audioFile, User user, String duration) {
         this.title = title;
         this.description = description;
-        this.filePath = filePath;
-        this.imagePath = imagePath;
+        this.audioFile = audioFile;
+        this.imageFile = imageFile;
         this.user = user;
         this.comments = new ArrayList<>();
         this.duration = duration;
@@ -37,23 +43,23 @@ public class Sound {
         this.categories = new ArrayList<>();
     }
 
-    public Sound(int id, String title, String description, String filePath, String imagePath, User user) {
+    public Sound(int id, String title, String description, byte[] imageFile, byte[] audioFile, User user) {
         this.title = title;
         this.id = id;
         this.description = description;
-        this.filePath = filePath;
-        this.imagePath = imagePath;
+        this.audioFile = audioFile;
+        this.imageFile = imageFile;
         this.user = user;
         this.uploadDate = LocalDateTime.now();
         this.categories = new ArrayList<>();
     }
 
-    public Sound(int id, String title, String description, String filePath, String imagePath, String duration) {
+    public Sound(int id, String title, String description,byte[] imageFile, byte[] audioFile, String duration) {
         this.title = title;
         this.id = id;
         this.description = description;
-        this.filePath = filePath;
-        this.imagePath = imagePath;
+        this.audioFile = audioFile;
+        this.imageFile = imageFile;
         this.duration = duration;
         this.uploadDate = LocalDateTime.now();
         this.categories = new ArrayList<>();
@@ -63,29 +69,45 @@ public class Sound {
 
     }
 
-    public Sound(int i, String title2, String description2, String audioPath, String imagePath2, User user, String duration2) {
+    public Sound(int i, String title2, String description2, byte[] imageFile, byte[] audioFile, User user, String duration2) {
         this.id = i;
         this.title = title2;
         this.description = description2;
-        this.filePath = audioPath;
-        this.imagePath = imagePath2;
+        this.audioFile = audioFile;
+        this.imageFile = imageFile;
         this.user = user;
         this.duration = duration2;
         this.uploadDate = LocalDateTime.now();
         this.categories = new ArrayList<>();
     }
 
-    public Sound(int i, String title, String description, String audioPath, String imagePath, User user, Category category, String duration) {
+    public Sound(int i, String title, String description, byte[] imageFile, byte[] audioFile, User user, Category category, String duration) {
         this.id = i;
         this.title = title;
         this.description = description;
-        this.filePath = audioPath;
-        this.imagePath = imagePath;
+        this.audioFile = audioFile;
+        this.imageFile = imageFile;
         this.duration = duration;
         this.uploadDate = LocalDateTime.now();
         this.categories = new ArrayList<>();
         this.categories.add(category);
         this.user = user;
+    }
+
+    public byte[] getAudioFile() {
+        return audioFile;
+    }
+
+    public void setAudioFile(byte[] audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public byte[] getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(byte[] imageFile) {
+        this.imageFile = imageFile;
     }
 
     public String getTitle() {
@@ -94,14 +116,6 @@ public class Sound {
 
     public String getDescription() {
         return description;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public String getImagePath() {
-        return imagePath;
     }
 
     public User getUser() {
@@ -130,14 +144,6 @@ public class Sound {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
     }
 
     public void setUser(User user) {
