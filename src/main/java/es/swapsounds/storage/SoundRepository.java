67ebbbf299da905ff4412717ddb07b05fdcc2c92
Sergio_Fdez-Davila <se.fernandezdav.2023@alumnos.urjc.es>
2025-04-08 10:50:54 +0200

    Necessart methods added to Sound class and Sound repository

--- src/main/java/es/swapsounds/storage/SoundRepository.java
+++ src/main/java/es/swapsounds/storage/SoundRepository.java
@@ -1,8 +1,11 @@
 package es.swapsounds.storage;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.swapsounds.model.Sound;
 
 public interface SoundRepository extends JpaRepository<Sound, Long> {
+    List<Sound> findByUserId(long userId);
 }
